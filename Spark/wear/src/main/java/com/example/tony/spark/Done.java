5e6e12fd0c36b9e3da2bae5ca08d00021af75a9e
Chang Liu <Cookmilk@MacBook-Pro.local>
2016-04-20 16:59:35 -0700

    Clean Watch

--- Spark/wear/src/main/java/com/example/tony/spark/Done.java
+++ Spark/wear/src/main/java/com/example/tony/spark/Done.java
@@ -8,7 +8,8 @@ public class Done extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_done);
+        setContentView(R.layout.activity_done); 
+        
     }
 
 }
