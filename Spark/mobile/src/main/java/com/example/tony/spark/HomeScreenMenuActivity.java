50f2a48af42473f38e54e167e140d76c18302780
Jeremy Diaz <j.jeremy.diaz@berkeley.edu>
2016-04-30 16:21:15 -0700

    menu links work

--- Spark/mobile/src/main/java/com/example/tony/spark/HomeScreenMenuActivity.java
+++ Spark/mobile/src/main/java/com/example/tony/spark/HomeScreenMenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.tony.spark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -66,18 +67,18 @@ public class HomeScreenMenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_home) {
+           //Do nothing
+        } else if (id == R.id.nav_analytics) {
+            startActivity(new Intent(this, AnalyticsActivity.class));
+        } else if (id == R.id.nav_diary) {
+            startActivity(new Intent(this, DiaryActivity.class));
+        } else if (id == R.id.nav_challenge) {
+            startActivity(new Intent(this, ChallengePage1Activity.class));
+        } else if (id == R.id.nav_about) {
+            startActivity(new Intent(this, AboutActivity.class));
+        } else if (id == R.id.nav_settings) {
+            startActivity(new Intent(this, Settings1Activity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
