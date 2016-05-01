b39e8082541b95ab45b94bd718be237ec2b64982
Chang Liu <liu.chang@berkeley.edu>
2016-04-30 21:18:59 -0700

    Finished Popup Windows

--- Spark/mobile/src/main/java/com/example/tony/spark/DiaryActivity.java
+++ Spark/mobile/src/main/java/com/example/tony/spark/DiaryActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class DiaryActivity extends AppCompatActivity {
 
@@ -12,35 +13,55 @@ public class DiaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
-        View myView = findViewById(R.id.imageButton);
+        Button today = (Button) findViewById(R.id.todaybut);
+        Button yesterday = (Button) findViewById(R.id.yestbut);
+        Button day = (Button) findViewById(R.id.daybut);
 
-        myView.setOnClickListener(new View.OnClickListener() {
+//        View myView = findViewById(R.id.imageButton);
+
+        today.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View arg0) {
-                startActivity(new Intent(DiaryActivity.this, DiaryEntryEditActivity.class));
+            public void onClick(View v) {
+                startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
             }
         });
-        myView.setOnTouchListener(new OnSwipeTouchListener(this) {
-            public boolean onSwipeTop() {
+
+        yesterday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
-                return true;
             }
-
-            public boolean onSwipeRight() {
-                startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
-                return true;
-            }
-
-            public boolean onSwipeLeft() {
-                startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
-                return true;
-            }
-
-            public boolean onSwipeBottom() {
-                return true;
-            }
-
         });
+
+        day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
+            }
+        });
+
+
+//        myView.setOnTouchListener(new OnSwipeTouchListener(this) {
+//            public boolean onSwipeTop() {
+//                startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
+//                return true;
+//            }
+//
+//            public boolean onSwipeRight() {
+//                startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
+//                return true;
+//            }
+//
+//            public boolean onSwipeLeft() {
+//                startActivity(new Intent(DiaryActivity.this, DiaryPopupActivity.class));
+//                return true;
+//            }
+//
+//            public boolean onSwipeBottom() {
+//                return true;
+//            }
+//
+//        });
     }
 
 }
--- Spark/mobile/src/main/java/com/example/tony/spark/DiaryPopupActivity.java
+++ Spark/mobile/src/main/java/com/example/tony/spark/DiaryPopupActivity.java
@@ -2,6 +2,8 @@ package com.example.tony.spark;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.widget.LinearLayout;
 
 public class DiaryPopupActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class DiaryPopupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_popup);
 
+//        LinearLayout dim = (LinearLayout) findViewById(R.id.)
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.widthPixels;
+
+        getWindow().setLayout((int)(width * .8),(int)(height * .8));
     }
 
 }
