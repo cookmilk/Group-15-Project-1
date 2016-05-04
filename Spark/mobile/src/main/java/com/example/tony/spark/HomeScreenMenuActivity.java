d7002d2f83234950b0841fa132c76b9e175d3598
Yiyang Shen <yshen@berkeley.edu>
2016-05-03 22:53:40 -0700

    add sleep/wake functionality

--- Spark/mobile/src/main/java/com/example/tony/spark/HomeScreenMenuActivity.java
+++ Spark/mobile/src/main/java/com/example/tony/spark/HomeScreenMenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.tony.spark;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,10 +15,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,6 +31,8 @@ import java.util.List;
 
 public class HomeScreenMenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    TextView e;
+    public static final String PREFS = "SleepOrWake";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,14 +90,62 @@ public class HomeScreenMenuActivity extends AppCompatActivity
         todayDate.setText(date);
 
         // To do: display entries
-//        SparkDataBase sdb = new SparkDataBase(this);
-//        List<ArrayList<String>> diarySummary = sdb.getDiarySummary(date);
-//        ArrayList<String> time = diarySummary.get(0);
-//        ArrayList<String> summary = diarySummary.get(1);
-//        int numEntries = time.size();
-//        if (numEntries > 3) {
-//
-//        }
+        LinearLayout entries =(LinearLayout) findViewById(R.id.entries);
+
+        final SparkDataBase sdb = new SparkDataBase(this);
+        List<ArrayList<String>> diarySummary = sdb.getDiarySummary(date);
+        ArrayList<String> time = diarySummary.get(0);
+        ArrayList<String> summary = diarySummary.get(1);
+        int numEntries = time.size();
+        List<TextView> entriesList = new ArrayList<TextView>();
+        for (int i = 0; i < Math.min(3, numEntries); i++) {
+            ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            TextView tv = new TextView(this);
+            tv.setLayoutParams(lparams);
+            String t = time.get(i);
+            String e = summary.get(i);
+            tv.setText(t + "   " + e);
+            entries.addView(tv);
+            entriesList.add(tv);
+        }
+        for (int j = 0; j < entriesList.size(); j++) {
+            e = entriesList.get(j);
+            e.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String[] timeEntry = e.getText().toString().split("   ");
+                    String t = timeEntry[0];
+                    Intent myIntent = new Intent(v.getContext(), DiaryEntryEditActivity.class);
+                    ArrayList<String> details = sdb.getSingleDiary(date, t);
+                    String[] detailsArr = new String[details.size()];
+                    detailsArr = details.toArray(detailsArr);
+                    myIntent.putExtra("details", detailsArr);
+                    myIntent.putExtra("date", date);
+                    myIntent.putExtra("time", t);
+                    v.getContext().startActivity(myIntent);
+                }
+            });
+        }
+
+        final ImageView sw = (ImageView) findViewById(R.id.sleep_wake);
+        final SharedPreferences sleep_wake = getSharedPreferences(PREFS, 0);
+        final SharedPreferences.Editor editor = sleep_wake.edit();
+        sw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean wake = sleep_wake.getBoolean("wake", true);
+                if (wake) {
+                    sw.setImageResource(R.drawable.wake);
+                    editor.putBoolean("wake", false);
+                    editor.commit();
+                } else {
+                    sw.setImageResource(R.drawable.sleep);
+                    editor.putBoolean("wake", true);
+                    editor.commit();
+                }
+            }
+        });
 
 
         newEntry.setOnClickListener(new View.OnClickListener() {
